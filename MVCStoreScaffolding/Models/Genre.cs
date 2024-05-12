96d0e7fbb7d96008c8e9993822a80d4f41dd55e0
Petar Dimitrievski <petar.dimitrievski.1@students.finki.ukim.mk>
2024-05-12 21:57:06 +0200

    Add project files.

---
+++ MVCStoreScaffolding/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCStoreScaffolding.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        [Display(Name = "Genre Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual List<Album> Albums { get; set; }
+    }
+}
